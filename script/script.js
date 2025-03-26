5ccff39bef522632633a9a1246fa92fc5c000622
MarcoNappa <marcomarconappa@gmail.com>
2025-03-26 01:58:24 +0100

    +

--- script/script.js
+++ script/script.js
@@ -1,7 +1,10 @@
 
 
-
+let titolo ="titolo2";
 
 document.getElementById("avviso").onclick = function (daiAvviso) {
     alert("Pagina in lavorazione...grazie per l'attesa e mi scuso del ritardo");
+};
+document.getElementById("saluto").onclick = function (saluta) {
+    alert("ciao");
 };
